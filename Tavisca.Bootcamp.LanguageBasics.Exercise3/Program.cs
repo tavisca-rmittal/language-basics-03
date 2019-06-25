f060043026f653570dd9e9479b69e69c26ba50bb
tavisca-rmittal <rmittal@tavisca.in>
2019-06-25 10:21:42 +0530

    Diet Plan Donegit add Program.cs

--- Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise3/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-
+using System.Collections.Generic;
 namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 {
     public static class Program
@@ -27,6 +27,8 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
                 new[] { 2, 6, 6, 2, 4, 4, 5, 0, 5, 5, 6, 6, 3, 5, 6 });
             Console.ReadKey(true);
         }
+                   
+
 
         private static void Test(int[] protein, int[] carbs, int[] fat, string[] dietPlans, int[] expected)
         {
@@ -38,10 +40,134 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine(result);
         }
 
+
+	public static int maxval(int[] prob,List<int> ls)
+        { 
+	  int m=prob[ls[0]];
+          if(ls.Count != 1)
+          {
+		foreach(int i in ls)
+          	{ 
+	          if(m<prob[i])
+            	  {
+                  m=prob[i];
+                  }
+          	}
+          }
+          return m;
+        }
+        public static int minval(int[] prob,List<int> ls)
+        {
+	  int m=prob[ls[0]];
+          if(ls.Count != 1)
+          {
+	     foreach(int i in ls)
+                { 
+		  if(m>prob[i])
+            	  {
+                  m=prob[i];
+                  }
+                }
+          }
+          return m;
+        }
+
+         public static List<int> Indexofall(int[] prob,List<int> ls,int val)
+         { 
+             List<int> resindex=new List<int>();
+             foreach(int x in ls)
+             { 
+                 if(prob[x]==val)
+                 { 
+                    resindex.Add(x);
+                 }
+             }
+             return resindex;
+
+         }
+	
+
         public static int[] SelectMeals(int[] protein, int[] carbs, int[] fat, string[] dietPlans)
         {
             // Add your code here.
-            throw new NotImplementedException();
+
+	    int l1=protein.Length;
+            int[] cal=new int[l1];
+            for(int i=0;i<l1;i++)
+            { 
+		cal[i]=protein[i]*5 + carbs[i]*5 + fat[i]*9;
+	    }   
+            int maxprotein=protein.Max();
+         
+            int maxfat=fat.Max();
+           
+            int maxcal=cal.Max();
+         
+            int[] res=new int[dietPlans.Length];
+            
+            for(int i=0;i<dietPlans.Length;i++)
+            {
+
+               List<int> ls = new List<int>();
+              for(int k=0;k<l1;k++)
+              { 
+                  ls.Add(k);
+              }
+
+
+              int maxv=0;
+              int minv=0;
+	      //arr index of dietplan have string like CpF.To convert every index into char arr
+              char[] arr=dietPlans[i].ToCharArray();
+              if(arr.Length==0)
+              {
+                  res[i]=0;
+                  continue;
+              }
+              for(int j=0;j<arr.Length;j++)
+              { 
+		  switch(arr[j])
+                      {
+                         case 'C': maxv=maxval(carbs,ls);
+                                   ls = Indexofall(carbs,ls,maxv);
+                                   break;
+		         case 'c': minv=minval(carbs,ls);
+                                   ls=Indexofall(carbs,ls,minv);
+                             	   break;
+                         case 'P': maxv=maxval(protein,ls);
+                             	   ls=Indexofall(protein,ls,maxv);
+                                   break;
+                         case 'p': minv=minval(protein,ls);
+                         	   ls=Indexofall(protein,ls,minv);
+                             	   break;
+                         case 'F': maxv=maxval(fat,ls);
+                                   ls=Indexofall(fat,ls,maxv);
+                             	   break;
+                    	 case 'f': minv=minval(fat,ls);
+                                   ls=Indexofall(fat,ls,minv);
+                                   break;
+                         case 'T': maxv=maxval(cal,ls);
+                                   ls=Indexofall(cal,ls,maxv);
+                             	   break;
+                    	 case 't': minv=minval(cal,ls);
+                             	   ls=Indexofall(cal,ls,minv);
+                             	   break;
+                      }
+                if(ls.Count==1)
+                {
+                     res[i]=ls[0];
+                     break;
+                }
+                
+              }
+              res[i]=ls[0];
+               
+                            
+            }
+          
+            //throw new NotImplementedException();
+           return res; 
         }
     }
 }
+          
